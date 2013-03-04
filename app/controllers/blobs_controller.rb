b96b69f4b597a490486d0f971344bcb614382875
gmarik <gmarik@gmail.com>
2013-03-03 20:33:53 -0600

    force content_type `text/plain` for blobs
    - to properly render raw output
    - closes #3

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -1,9 +1,6 @@
 class BlobsController < ApplicationController
-  respond_to :html, :text
-
   def show
     @gist = Gist.find(params[:gist_id])
-    #render text: @gist.repo.lookup(params[:id]).read_raw.data, :layout => false
-    render :inline => "<html><body style=\"white-space:pre-wrap\">" + @gist.repo.lookup(params[:id]).read_raw.data + "</body></html>"
+    render text: @gist.repo.lookup(params[:id]).read_raw.data, :layout => false, :content_type => 'text/plain'
   end
 end
